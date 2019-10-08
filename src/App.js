706df65e1dc3f538b4d645350df3bfe25d224c87
Mark Mathews <marklmathews4@gmail.com>
2019-10-08 11:16:04 -0600

    finished frontend contact page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Contact from './components/Contact/Contact'
+import './Reset.css'
 
 function App() {
   return (
     <div className="App">
-	App
+      <Contact/>
     </div>
   );
 }
