2e8d154cb7c1244a5f417c65956da82ac4509243
McKay Nelson <tmckaynelson@gmail.com>
2019-10-09 15:14:40 -0600

    sql query was getting both company and warehouse info

--- db/get_company_info_by_warehouse.sql
+++ db/get_company_info_by_warehouse.sql
@@ -1,3 +1,3 @@
-SELECT * FROM companies
-JOIN warehouses ON warehouses.company_id = companies.company_id
-WHERE warehouses.company_id = $1;
+SELECT c.name, c.address, c.city, c.state, c.zip, c.owner_id FROM companies AS w
+JOIN warehouses AS w ON w.company_id = c.company_id
+WHERE w.company_id = $1;
