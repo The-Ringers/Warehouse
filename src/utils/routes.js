e4aa05fe6f1c9aef155312fc6d66dc4c47a79f59
JMGino <JMGino909@gmail.com>
2019-10-10 10:23:07 -0600

    added inventory folder and route

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -4,7 +4,8 @@ import { Switch, Route } from 'react-router-dom';
 import Landing from '../components/Landing/Landing';
 import Dashboard from '../components/Dashboard/Dashboard';
 import Contact from '../components/Contact/Contact';
-import Invoice from '../components/Invoice/Invoice'
+import Invoice from '../components/Invoice/Invoice';
+import Inventory from '../components/Inventory/Inventory';
 
 export default (
     <Switch>
@@ -12,5 +13,6 @@ export default (
         <Route path='/contact' component={Contact} />
         <Route path='/:company/dashboard' component={Dashboard}/>
         <Route path='/:company/invoice' component={Invoice}/>
+        <Route path='/:company/inventory' component={Inventory}/>
     </Switch>
 )
