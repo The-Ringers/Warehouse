d4bca7ab4020d871dda5bc7c9612dbbaf87e4b07
McKay Nelson <tmckaynelson@gmail.com>
2019-10-10 09:40:57 -0600

    more necessary sql files

---
+++ db/delete_sale_details.sql
@@ -0,0 +1,2 @@
+DELETE FROM sale_details
+WHERE sale_details_id = $1
--- db/edit_sales.sql
+++ db/edit_sales.sql
@@ -1,1 +1,3 @@
 UPDATE sales
+SET category = $1, payment = $2, subtotal = $3, tax = $4, total = $5, pdf = $6
+WHERE sales_id = $7;
