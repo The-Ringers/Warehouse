459d566a298b9090088c558896f89d05bbcb585f
justsomeguy <thenaders3@gmail.com>
2019-10-15 16:32:02 -0600

    changed sales sql files and added state variables to invoice.js

--- db/seeds/shipping_delivery.sql
+++ db/seeds/shipping_delivery.sql
@@ -4,5 +4,6 @@ CREATE TABLE shipping_delivery (
     city VARCHAR(40),
     state VARCHAR(2),
     zip VARCHAR(15),
-    sales_id INTEGER REFERENCES sales(sales_id)
-)
+    sales_id INTEGER REFERENCES sales(sales_id),
+    type shipping_delivery_type 
+); 
