1458848e76bdd5dc379d82d69e4aa43ed70a06b1
Mark Mathews <marklmathews4@gmail.com>
2019-10-15 16:41:06 -0600

    finished order

--- src/components/Invoice/Invoice.js
+++ src/components/Invoice/Invoice.js
@@ -11,15 +11,52 @@ import axios from 'axios';
 import { TextField } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import { useSelector } from 'react-redux';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Typography from '@material-ui/core/Typography';
+import Radio from '@material-ui/core/Radio';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel'
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
-
+const theme = createMuiTheme ({
+  overrides: {
+    MuiStepIcon: {
+      root: {
+        '&$completed': {
+          color: '#640d0d',
+        },
+        '&$active': {
+          color: '#947C7C',
+        },
+      }
+    },
+    PrivateRadioButtonIcon: {
+      root: {
+        '&$checked': {
+          color: '#640d0d',
+        },
+        '&$unchecked': {
+          color: '#947C7C',
+        },
+      }
+    }
+  }
+})
 const useStyles = makeStyles(theme => ({
   paper: {
     width: 'calc(100% - 300px)',
     display: 'flex',
+    flexDirection: 'column',
     flexWrap: 'wrap',
     float: 'right',
-    marginRight: '5px',
   },
   
   table: {
@@ -39,17 +76,24 @@ const useStyles = makeStyles(theme => ({
   },
   Button: {
     width: '100px',
-    background: 'rgb(500,200,200)',
+    background: '#640D0D',
     margin: '15px',
-    fontFamily: 'Alegreya Sans SC, sans-serif'
+    fontFamily: 'Alegreya Sans SC, sans-serif',
+    color: 'white'
   },
-  Button1: {
-    width: '500px',
-    background: 'rgb(500,200,200)',
-    fontFamily: 'Alegreya Sans SC, sans-serif'
+  submitButton: {
+    width: '100px',
+    background: '#640D0D',
+    fontFamily: 'Alegreya Sans SC, sans-serif',
+    color: 'white',
+    alignSelf: 'flex-end',
+    marginRight: '10px',
+    marginTop: '40px',
+    marginBottom: '10px'
   },
   taxField: {
-    width: '60px'
+    width: '60px',
+    
   },
   icon: {
     width: '30px',
@@ -61,10 +105,55 @@ const useStyles = makeStyles(theme => ({
   },
   TableRow: {
     fontFamily: 'Alegreya Sans SC, sans-serif'
-
+  },
+  modalpaper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(3, 4, 3),
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: 'white',
+  },
+  instructions: {
+    margin: '10px',
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(3)
+  },
+  modalInputs: {
+    padding: '100px',
+    display: 'flex',
+    flexDirection: 'column'
+    
+  },
+  root: {
+    width: '100%',
+  },
+  backButton: {
+    width: '150px',
+    alignSelf: 'center'
+  },
+  modalInput: {
+    width: '500px',
+    margin: '10px',
+  },
+  nextButton: {
+    width: '150px',
+    background: '#640D0D',
+    color: 'white',
+    alignSelf: 'center'
+  },
+  stepper: {
+    background: '#640D0D'
   }
   
 }));
+
 
 const calculateSubtotal = (items) => {
   return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
@@ -75,6 +164,18 @@ export default function Invoice(props) {
   const [sku, setSku] = useState('');
   const [taxRate, setTaxRate] = useState(0);
   const [itemList, setItemList] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [activeStep, setActiveStep] = useState(0);
+  const [selectedValue, setSelectedValue] = useState('a')
+  const [values, setValues] = useState('')
+  const [category] = useState('invoice')
+  const [paymentType, setPaymentType] = useState('');
+  const [first_name, setFirst_name] = useState('');
+  const [last_name, setLast_name] = useState('');
+  const [company, setCompany] = useState(''); 
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  
   const subtotal = calculateSubtotal(itemList);
   const tax = (taxRate/100) * subtotal;
   const total = tax + subtotal;
@@ -113,6 +214,7 @@ export default function Invoice(props) {
       let newArray = itemList.slice();
       newArray.push(addItem(sku, description, qty, unit));
       setItemList(newArray);
+      console.log(itemList)
     })
     .catch((error) => {
       console.log(error)
@@ -133,34 +235,118 @@ export default function Invoice(props) {
     setItemList(newArray)
   };
 
-  // const submitInvoice = () => {
-  //   const invoiceObject = {
-  //     warehouse_id,
-  //     company_id, 
-  //     // user_id, being pulled off the session. 
+  const submitSale = () => {
+    const saleObject = {
+      warehouse_id,
+      user_id,
+      company_id, 
+      category, 
+      subtotal,
+      tax, 
+      total, 
+      paymentType
+      // pdf
+    }; 
 
-  //     // FIXME: not sure where to get customer_id from
-  //     // customer_id, 
-  //     category: 'invoice', 
-  //     subtotal,
-  //     tax, 
-  //     total, 
-  //     // payment, 
-  //     // pdf
-  //   }; 
+    // const shippingObject = {
+    //   address,
+    //   city,
+    //   state,
+    //   zip 
+    // };
 
-  //   const sale_details = itemList; 
+    const sale_details = itemList; 
 
-  //   axios.post('/api/sales', {invoiceObject, sale_details})
-  //     .then(response => {
-  //       console.log(response)
-  //     })
-  //     .catch(err => console.log(err))
-  // }; 
+    axios.post('/api/sales', {saleObject, sale_details})
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => console.log(err))
+  }; 
+  const handleChange = event => {
+    setSelectedValue(event.target.value);
+  };
+  const handleClick = event => {
+    setValues(event.target.value)
+    }
+
+  function getSteps() {
+    return ['Customer Information', 'Payment/Process', 'Shipping/Delivery'];
+  }
+  
+  const case0 = () => {
+    return  <div className={classes.modalInputs} >
+              <TextField className={classes.modalInput} label='First Name' variant='filled' ></TextField>
+              <TextField className={classes.modalInput} label='Last Name' variant='filled' ></TextField>
+              <TextField className={classes.modalInput} label='Company' variant='filled' ></TextField>
+              <TextField className={classes.modalInput} label='Email' variant='filled' ></TextField>
+              <TextField className={classes.modalInput} label='Phone #' variant='filled' ></TextField>
+            </div>
+  }
+  const case1 = () => {
+    return  <div className={classes.modalInputs}>
+              <InputLabel>Payment Type</InputLabel>
+              <Select value={values} onChange={handleClick} inputProps={{ name: 'payment',}} placeholder='Payment Type'>
+                <MenuItem value='Credit'>Credit</MenuItem>
+                <MenuItem value='Cash'>Cash</MenuItem>
+                <MenuItem value='Check'>Check</MenuItem>
+              </Select>
+            </div>
+  }
+  const case2 = () => {
+    return  <div className={classes.modalInputs}>
+              <TextField className={classes.modalInput} label='Address' variant='filled' ></TextField>
+              <TextField className={classes.modalInput} label='City' variant='filled' ></TextField>
+              <TextField className={classes.modalInput} label='State' variant='filled' ></TextField>
+              <TextField className={classes.modalInput} label='Zipcode' variant='filled' ></TextField>
+              <FormControlLabel checked={selectedValue === 'Delivery'} onChange={handleChange} value="Delivery" label='Delivery' control={<Radio color="primary" />} labelPlacement="start"/>
+              <FormControlLabel checked={selectedValue === 'Shipping'} onChange={handleChange} value="Shipping" label='Shipping' control={<Radio color="primary" />} labelPlacement="start"/>
+            </div>
+  }
+  
+  function getStepContent(stepIndex) {
+    switch (stepIndex) {
+      case 0:
+        return case0();
+      case 1:
+        return case1();
+      case 2:
+        return case2();
+      default:
+        return case0();
+    }
+  }
+  const steps = getSteps();
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+    if(activeStep === steps.length -1)
+    return 
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+
+  
+  const handleSubmit = () => {
+    setOpen(false);
+    setActiveStep(0);
+  }
+
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
+    <ThemeProvider theme={theme}>
     <Paper className={classes.paper}>
-      <TextField onChange={e => setSku(e.target.value)} label='Search...' type='search' id='filled-search' className={classes.TextField}></TextField>
+      <TextField onChange={e => setSku(e.target.value)} label='Search Inventory...' type='search' id='filled-search' className={classes.TextField}></TextField>
       <Button className={classes.Button} onClick={getInventory}>Add Item</Button>
       <Table className={classes.table}>
         <TableHead>
@@ -203,7 +389,58 @@ export default function Invoice(props) {
           </TableRow>
         </TableBody>
       </Table>
-      <Button className={classes.Button1} onClick={''}>Submit</Button>
-    </Paper>
+      <Button className={classes.submitButton} onClick={handleOpen}>Next</Button>
+      <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.modalpaper}>
+          <div className={classes.root}>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map(label => (
+          <Step key={label}>h
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography className={classes.instructions}>All steps completed</Typography>
+          </div>
+        ) : (
+          <div>
+            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+            <div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+              <Button className={classes.nextButton} variant="contained" onClick={activeStep === steps.length - 1 ? handleSubmit : handleNext}>
+                {activeStep === steps.length - 1 ? 'Submit' : 'Next'}
+              </Button>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.backButton}
+              >
+                Back
+              </Button>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+    </Paper></ThemeProvider>
   );
 };
