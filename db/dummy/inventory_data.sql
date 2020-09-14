7daecf9c6d42c53c72f83598e4b6546911dfbeb3
McKay Nelson <tmckaynelson@gmail.com>
2020-09-14 16:07:39 -0600

    updated npm

--- db/dummy/inventory_data.sql
+++ db/dummy/inventory_data.sql
@@ -1,44 +1,44 @@
 INSERT INTO inventory
 (warehouse_id, price, quantity, sku, description, category)
 VALUES
-(4, 1.15, 5, 00000001, 'paper clips, box, 100pcs', 'office supplies');
+(6, 235, 16, 'm13', 'GTX 1050 5GB, single fan', 'Graphics Card' );
 
 INSERT INTO inventory
 (warehouse_id, price, quantity, sku, description, category)
 VALUES
-(4, .5, 5, 00000002, 'pen, 1pc', 'office supplies');
+(6, 800, 5, 'm14', 'RTX 2080 8GB, triple fan single fan', 'Graphics Card' );
 
 INSERT INTO inventory
 (warehouse_id, price, quantity, sku, description, category)
 VALUES
-(4, 2.35, 5, 00000003, 'paper, box, 4reams', 'office supplies');
+(6, 750, 10, 'm15', 'RTX 2080 8GB, double fan', 'Graphics Card' );
 
 INSERT INTO inventory
 (warehouse_id, price, quantity, sku, description, category)
 VALUES
-(4, 600, 5, 00000004, 'concrete saw', 'tools');
+(6, 199, 33, 'm10', 'AMD Ryzen 5 3600, 6-core 3.6GHz', 'Processors' );
 
 INSERT INTO inventory
 (warehouse_id, price, quantity, sku, description, category)
 VALUES
-(4, 115, 5, 00000005, 'impact driver', 'tools');
+(6, 500, 21, 'm11', 'AMD Ryzen 7 3900, 12-core 3.8GHz', 'Processors' );
 
 INSERT INTO inventory
 (warehouse_id, price, quantity, sku, description, category)
 VALUES
-(4, 100, 5, 00000006, 'cordless drill', 'tools');
+(6, 500, 13, 'm12', 'Intel i9900k, 8-core 3.8GHz', 'Processors' );
 
 INSERT INTO inventory
 (warehouse_id, price, quantity, sku, description, category)
 VALUES
-(4, 4, 5, 00000007, 'nails, box, 100pcs, 2.5"', 'hardware');
+(6, 12, 200, 'm7', 'Amazon Basics mouse', 'Accessories' );
 
 INSERT INTO inventory
 (warehouse_id, price, quantity, sku, description, category)
 VALUES
-(4, 5, 5, 00000008, 'nails, box, 100pcs, 3.5"', 'hardware');
+(6, 69.99, 50, 'm8', 'Logitech g502 Hero, 11 programmable buttons and the new 16,000 dpi Hero sensor', 'Accessories' );
 
 INSERT INTO inventory
 (warehouse_id, price, quantity, sku, description, category)
 VALUES
-(4, 3.45, 5, 00000009, 'nails, box, 100pcs, 1"', 'hardware');
+(6, 167.90, 17, 'm9', 'DasKeyboard Ultimate for Mac', 'Accessories' );
